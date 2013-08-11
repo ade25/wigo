bc879abf8e09abdc6edd4a5a186bb9c8aff0b5ac
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-08-11 20:41:38 +0200

    Update settings with plone.api and five.pt

--- src/wigo.statusapp/setup.py
+++ src/wigo.statusapp/setup.py
@@ -26,6 +26,7 @@ setup(name='wigo.statusapp',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'plone.api',
           'plone.app.dexterity [grok]',
           'plone.namedfile [blobs]',
           # -*- Extra requirements: -*-
--- src/wigo.statusapp/wigo/statusapp/statusview.py
+++ src/wigo.statusapp/wigo/statusapp/statusview.py
@@ -1,9 +1,23 @@
 from five import grok
+from plone import api
 
 from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.app.contentlisting.interfaces import IContentListing
+
+from wigo.statusapp.component import IComponent
 
 
 class StatusView(grok.View):
     grok.context(INavigationRoot)
     grok.require('zope2.View')
     grok.name('status-quo')
+
+    def update(self):
+        self.has_components = len(self.available_components()) > 0
+
+    def available_components(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IComponent.__identifier__,
+                        review_state='published')
+        results = IContentListing(items)
+        return results
