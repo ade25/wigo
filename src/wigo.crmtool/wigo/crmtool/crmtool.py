0818407f24c6db1aade2ae8784cb65ac982cea83
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-10-23 10:46:54 +0200

    Add crm tool and styles

--- src/wigo.crmtool/wigo/crmtool/crmtool.py
+++ src/wigo.crmtool/wigo/crmtool/crmtool.py
@@ -20,48 +20,19 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from wigo.crmtool import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
 class ICRMTool(form.Schema, IImageScaleTraversable):
     """
     CRM Tool holding information on hosting customers
     """
 
-    # If you want a schema-defined interface, delete the model.load
-    # line below and delete the matching file in the models sub-directory.
-    # If you want a model-based interface, edit
-    # models/crmtool.xml to define the content type.
-
-    form.model("models/crmtool.xml")
-
-
-# Custom content-type class; objects created for this content type will
-# be instances of this class. Use this class to add content-type specific
-# methods and properties. Put methods that are mainly useful for rendering
-# in separate view classes.
 
 class CRMTool(Container):
     grok.implements(ICRMTool)
 
-    # Add your class methods and properties here
 
-
-# View class
-# The view will automatically use a similarly named template in
-# crmtool_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
-    """ sample view class """
-
+class View(grok.View):
     grok.context(ICRMTool)
     grok.require('zope2.View')
-
-    # grok.name('view')
+    grok.name('view')
 
     # Add view methods here
