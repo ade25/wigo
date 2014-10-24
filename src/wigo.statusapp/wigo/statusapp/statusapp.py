2587f6301929cda3e0f2c30500cfca4b3ef92d4a
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-10-25 01:18:47 +0200

    Add cluster info builder
    We store the whole cluster information as json file. The structure
    is available as cluster.json

--- src/wigo.statusapp/wigo/statusapp/statusapp.py
+++ src/wigo.statusapp/wigo/statusapp/statusapp.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """Module providing status app overview and cluster storage"""
 
+import datetime
+import time
 import json
+
+import uuid as uuid_tool
 
 from AccessControl import Unauthorized
 from Acquisition import aq_inner
@@ -262,3 +266,72 @@ class TransitionState(grok.View):
         came_from = api.content.get(UID=uuid)
         next_url = came_from.absolute_url()
         return self.request.response.redirect(next_url)
+
+
+class BuildClusterInfo(grok.View):
+    grok.context(IStatusApp)
+    grok.require('cmf.ModifyPortalContent')
+    grok.name('build-cluster-info')
+
+    def get_components(self):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IComponent.__identifier__,
+                        sort_on='getObjPositionInParent')
+        return items
+
+    def get_item_data(self, item):
+        obj = item.getObject()
+        hostname = getattr(obj, 'server')
+        tool = getUtility(IWigoTool)
+        info = {}
+        info['id'] = obj.Title()
+        info['uid'] = uuid_tool.uuid4()
+        info['component'] = api.content.get_uuid(obj=obj)
+        info['created'] = datetime.datetime.now(),
+        info['hostname'] = hostname
+        info['ip'] = getattr(obj, 'ip')
+        info['server'] = getattr(obj, 'machine')
+        try:
+            data = tool.get(hostname=hostname)
+            info['serverdetails'] = True
+            info['id'] = data['server']
+            info['hostname'] = data['servername']
+            info['components'] = {
+                'nginx': data['nginx'],
+                'varnish': data['varnish'],
+                'haproxy': data['haproxy']
+            }
+            info['sites'] = data['sites']
+        except:
+            info['serverdetails'] = False
+        return info
+
+    def get_items(self):
+        results = self.get_components()
+        items = []
+        for r in results:
+            item = self.get_item_data(r)
+            items.append(item)
+        return items
+
+    def _process_request(self):
+        api_url = self.request.get('ACTUAL_URL')
+        data = {
+            'url': api_url,
+            'timestamp': int(time.time()),
+        }
+        if self.valid_token():
+            items = self.get_items()
+            data['count'] = len(items)
+            data['items'] = items
+        else:
+            data['error'] = u"Access token invalid or missing from request"
+        return data
+
+    def render(self):
+        start = time.time()
+        data = self._process_request()
+        end = time.time()
+        data.update(dict(_runtime=end-start))
+        self.request.response.setHeader("Content-Type", "application/json")
+        return json.dumps(data)
